80980e7fcbf984dae58efee39afe0df3563624ff
ahmedrimon <ahmedrimon@gmail.com>
2021-06-14 22:15:14 +0600

    Rimons file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import playersData from './data/db.json'
+
 
 function App() {
-  
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    setPlayers(playersData);
+    console.log(playersData);
+  }, [])
   return (
     <div>
-      <h1>America</h1>
-      
+      <Header></Header>
+      <ul>
+        {
+          players.map(player => <li>{player.name}</li>)
+        }
+      </ul>
+      <h2>This profile picture: {players.image}</h2>
     </div>
   );
 }
